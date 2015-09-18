0763f0040fc6e496f9652f81402662e9e1865fb5
Ian Hoppes <i@iginteractive.com>
2015-09-17 22:13:00 -0400

    Added support for TMP36 sensor

---
+++ SleepyBaby.RaspberryPi/Sensors/TMP36.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Spi;
+using Windows.System.Threading;
+
+namespace SleepyBaby.RaspberryPi.Sensors
+{
+    class Tmp36
+    {
+        private readonly SpiDevice _mcp3008;
+        private readonly int _channel;
+        private readonly ThreadPoolTimer _dataReadTimer;
+
+        public event EventHandler<SensorEventArgs> DataReadEvent;
+
+        public Tmp36(SpiDevice mcp3008, int channel, TimeSpan dataReadInterval)
+        {
+            this._mcp3008 = mcp3008;
+            this._channel = channel;
+            this._dataReadTimer = ThreadPoolTimer.CreatePeriodicTimer(DataReadTimer_Tick, TimeSpan.FromSeconds(10));
+        }
+
+        private void DataReadTimer_Tick(ThreadPoolTimer timer)
+        {
+            var channelByte = (byte)((8 + _channel) << 4);
+            var transmitBuffer = new byte[3] { 1, channelByte, 0x00 };
+            var receiveBuffer = new byte[3];
+
+            _mcp3008.TransferFullDuplex(transmitBuffer, receiveBuffer);
+            //first byte returned is 0 (00000000), 
+            //second byte returned we are only interested in the last 2 bits 00000011 (mask of &3) 
+            //then shift result 8 bits to make room for the data from the 3rd byte (makes 10 bits total)
+            //third byte, need all bits, simply add it to the above result 
+            var sensorData = ((receiveBuffer[1] & 3) << 8) + receiveBuffer[2];
+            //TMP36 == 10mV/1degC ... 3.3V = 3300.0 mV, 10 bit chip # steps is 2 exp 10 == 1024
+            var millivolts = sensorData * (3300.0 / 1024.0);
+            var tempC = (millivolts - 500) / 10;
+            var tempF = Math.Round((tempC * 9.0 / 5.0) + 32, 1);
+
+            if (DataReadEvent != null)
+                DataReadEvent(this, new SensorEventArgs { Value = tempF });
+        }
+    }
+}
--- SleepyBaby.RaspberryPi/StartupTask.cs
+++ SleepyBaby.RaspberryPi/StartupTask.cs
@@ -4,18 +4,60 @@ using System.Linq;
 using System.Text;
 using System.Net.Http;
 using Windows.ApplicationModel.Background;
+using Windows.Devices.Gpio;
+using Windows.Devices.Spi;
+using Windows.System.Threading;
+using Windows.Devices.Enumeration;
+using System.Diagnostics;
+using SleepyBaby.RaspberryPi.Sensors;
 
 // The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
 
 namespace SleepyBaby.RaspberryPi
-{
+{   
     public sealed class StartupTask : IBackgroundTask
     {
+        private SpiDevice _mcp3008;
+        private Tmp36 _tmp36;
+
         public void Run(IBackgroundTaskInstance taskInstance)
         {
-            // 
-            // TODO: Insert code to start one or more asynchronous methods 
-            //
+            InitMcp3008();
+            InitSensors();
+            while (true)
+            {
+
+            }
+        }
+
+        private async void InitMcp3008()
+        {
+            //using SPI0 on the Pi
+            var spiSettings = new SpiConnectionSettings(0); //for spi bus index 0
+            spiSettings.ClockFrequency = 10000; //10kHz-3.6MHz
+            spiSettings.Mode = SpiMode.Mode0;
+
+            var spiQuery = SpiDevice.GetDeviceSelector("SPI0");
+            var deviceInfo = await DeviceInformation.FindAllAsync(spiQuery);
+            if (deviceInfo != null && deviceInfo.Count > 0)
+            {
+                _mcp3008 = await SpiDevice.FromIdAsync(deviceInfo[0].Id, spiSettings);
+            }
+            else
+            {
+                throw new InvalidOperationException("No MCP3008 found at SPI0");
+            }
+        }
+
+        private void InitSensors()
+        {
+            _tmp36 = new Tmp36(_mcp3008, 0, TimeSpan.FromSeconds(10));
+            _tmp36.DataReadEvent += Tmp36_DataReadEvent;
+        }
+
+        private void Tmp36_DataReadEvent(object sender, SensorEventArgs e)
+        {
+            Debug.WriteLine("Tmp36: " + e.Value);
         }
     }
 }
